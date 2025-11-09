8541f132a88b150470dba94e82cb4ab5714d9f50
XDPXI <119396780+XDPXI@users.noreply.github.com>
2025-11-09 18:34:32 +1100

    Upload

---
+++ src/main/java/com/yourname/yourplugin/YourPlugin.java
@@ -0,0 +1,52 @@
+package com.yourname.yourplugin;
+
+import dev.xdpxi.swiftmc.api.plugin.Plugin;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.minestom.server.command.builder.Command;
+import net.minestom.server.entity.Player;
+
+public class YourPlugin extends Plugin {
+
+    @Override
+    public void onLoad() {
+        // Called when the plugin is loaded (before enabling)
+        logInfo("Plugin is being loaded...");
+
+        // Save default config if it doesn't exist
+        saveDefaultConfig();
+    }
+
+    @Override
+    public void onEnable() {
+        // Called when the plugin is enabled
+        logInfo("Plugin has been enabled!");
+
+        // Register a command
+        registerCommand(new HelloCommand());
+
+        logInfo("Commands and events registered successfully!");
+    }
+
+    @Override
+    public void onDisable() {
+        // Called when the plugin is disabled
+        logInfo("Plugin has been disabled. Goodbye!");
+    }
+
+    // Custom command
+    private class HelloCommand extends Command {
+        public HelloCommand() {
+            super("hello");
+
+            setDefaultExecutor((sender, context) -> {
+                if (sender instanceof Player player) {
+                    player.sendMessage(Component.text("Hello from ExamplePlugin!", NamedTextColor.GREEN));
+                    player.sendMessage(Component.text("Author: " + getAuthor(), NamedTextColor.GRAY));
+                } else {
+                    sender.sendMessage(Component.text("Only players can use this command!"));
+                }
+            });
+        }
+    }
+}
